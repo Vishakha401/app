e74b051dafc68d8725a497279c95812e9bc329fb
Vishakha Sonmore <vsonmore@gmail.com>
2023-12-04 22:59:39 -0800

    home page completed

---
+++ Vishakha_sonmore/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Vishakha_sonmore project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Vishakha_sonmore.settings')
+
+application = get_asgi_application()
