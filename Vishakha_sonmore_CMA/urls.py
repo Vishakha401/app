4103348cdad34d0ec4b538ac09405cf4118334b8
Vishakha Sonmore <vsonmore@gmail.com>
2023-12-10 23:09:50 -0800

    homepage edited- background and buttons

--- Vishakha_sonmore_CMA/urls.py
+++ Vishakha_sonmore_CMA/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('ref', views.about, name='ref'),
     path('addContact', views.addContact, name='addContact'),
     path('details/<contact_id>', views.details, name='details'),
     path('update/<contact_id>', views.update, name='update'),
